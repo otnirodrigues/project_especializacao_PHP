3e1301de6158274b9ca611ffd253903915998340
@otni.rodrigues <ors@cin.ufpe.br>
2022-12-25 22:38:45 -0300

    show agency

--- src/Controller/AgenciaController.php
+++ src/Controller/AgenciaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AgenciaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class AgenciaController extends AbstractController
 {
     #[Route('/agencia', name: 'app_agencia')]
-    public function index(): Response
+    public function index(AgenciaRepository $agencias): Response
     {
+    
         return $this->render('agencia/index.html.twig', [
             'controller_name' => 'AgenciaController',
+            'agencias' => $agencias->findAll()
         ]);
     }
 }
